3620e6253af68f6a00734acaafe567b4c80c1842
Vinicius Monteiro <vinicius3_1998@hotmail.com>
2025-09-19 19:59:31 -0300

    fix(#14): Resetando a branch

--- swag/swag/model/Project.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Project(models.Model):
-    id_project = models.IntegerField()
-    key_project = models.IntegerField()
-    nome_project = models.CharField(max_length=100)
-    type_project = models.CharField(max_length=50)
