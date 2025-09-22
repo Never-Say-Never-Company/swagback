54e0504db066162c2cfbfd306b3058e4fe66f87b
Vinicius Monteiro <vinicius3_1998@hotmail.com>
2025-09-22 01:03:32 -0300

    feat(#14): criado métodos para salvar por projeto, pegar projeto por período e por autor

--- swag/nsnapp/views.py
+++ swag/nsnapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from requests.auth import HTTPBasicAuth
 from decouple import config
-import requests
 from pymongo import MongoClient
+from datetime import datetime, timedelta
+from nsnapp.utils import convert_objectid_to_str
+import requests
+import json
 
 API_URL_PROJECT = config("JIRA_URL_PROJECT")
 API_URL_ISSUES = config("JIRA_URL_ISSUES")
@@ -45,6 +49,7 @@ def clean_data(project, issue):
     for w in worklogs:
         author_logs.append({
             "display_name": w.get("author", {}).get("displayName"),
+            "jira_created_at": datetime.strptime(w.get("created").split(".")[0], "%Y-%m-%dT%H:%M:%S"),
             "time_spent": w.get("timeSpent"),
             "time_spent_seconds": w.get("timeSpentSeconds"),
         })
@@ -62,29 +67,95 @@ def clean_data(project, issue):
         ]
     }
 
-def save_data():
-    project_data = get_api_data_project()
-    issues_data = get_api_data_issues()
-    issues_list = issues_data.get("issues", [])
+@csrf_exempt
+def save_data(request):
+    if request.method == "POST":
+        project_data = get_api_data_project()
+        issues_data = get_api_data_issues()
+        issues_list = issues_data.get("issues", [])
 
-    try:
-        if isinstance(project_data, list) and isinstance(issues_list, list):
-            cleaned_data = []
-            for project in project_data:
-                for issue in issues_list:
-                    worklogs = issue.get("fields", {}).get("worklog", {}).get("worklogs", [])
-                    if worklogs:
-                        issue_key = issue["key"]
-                        issue_key = issue_key.split("-")[0]
-                        
-                        if project["key"] == issue_key:
-                            cleaned_data.append(clean_data(project, issue))
-            project_collections.drop()
-            project_collections.insert_many(cleaned_data)
-        elif isinstance(project_data, dict) and isinstance(issues_data, dict):
-            cleaned_project_data = clean_data(project_data, issues_data)
-            project_collections.insert_one(cleaned_project_data)
-    except requests.exceptions.RequestException as e:
-        return requests.Response.json()
+        try:
+            if isinstance(project_data, list) and isinstance(issues_list, list):
+                cleaned_data = []
+                for project in project_data:
+                    for issue in issues_list:
+                        worklogs = issue.get("fields", {}).get("worklog", {}).get("worklogs", [])
+                        if worklogs:
+                            issue_key = issue["key"]
+                            issue_key = issue_key.split("-")[0]
+                            
+                            if project["key"] == issue_key:
+                                cleaned_data.append(clean_data(project, issue))
+                                #print(cleaned_data)
+                if project_collections.name in project_collections.database.list_collection_names():
+                    project_collections.drop()
+                project_collections.insert_many(cleaned_data)
+                
+                return JsonResponse({"status": "success", "message": "Dados salvos"})
+            elif isinstance(project_data, dict) and isinstance(issues_data, dict):
+                cleaned_project_data = clean_data(project_data, issues_data)
+                project_collections.insert_one(cleaned_project_data)
+        except requests.exceptions.RequestException as e:
+            return requests.Response.json()
     
-save_data()
+def get_project_by_period(begin, end):    
+    begin_formated = datetime.strptime(begin, "%Y-%m-%d")
+    end_formated = datetime.strptime(end, "%Y-%m-%d") + timedelta(days=1) - timedelta(seconds=1)
+
+    results = project_collections.find({
+        "issues": {
+            "$elemMatch": {
+                "author_logs": {
+                    "$elemMatch": {
+                        "jira_created_at": {
+                            "$gte": begin_formated,
+                            "$lte": end_formated
+                        }
+                    }
+                }
+            }
+        }
+    })
+    finded_projects = list(results)
+
+    return finded_projects
+
+@csrf_exempt
+def get_project_per_period(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+
+            if isinstance(data, list):
+                data = data[0]
+
+            begin = data.get("begin")
+            end = data.get("end")
+
+            if not begin or not end:
+                return JsonResponse({"error": "Necessário ter 'begin' e 'end' nas chaves."}, status=400)
+            
+            result = list(get_project_by_period(begin, end))
+            result = convert_objectid_to_str(result)
+            
+            return JsonResponse(result, safe=False)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "JSON inválido"}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+def get_project_by_author(author_name):
+    results = project_collections.find({
+        "issues": {
+            "$elemMatch": {
+                "author_logs": {
+                    "$elemMatch": {
+                        "display_name": author_name
+                    }
+                }
+            }
+        }
+    })
+    finded_projects = list(results)
+
+    return finded_projects
