d0dabfe50efd2fa288eee39a1d94d25b6c6d930e
Paloma <paloma.soaresrocha@gmail.com>
2025-09-27 16:53:47 -0300

    feat(#16): salva e lista os usuarios da API do jira

--- swag/nsnapp/views.py
+++ swag/nsnapp/views.py
@@ -12,6 +12,7 @@ import os
 
 API_URL_PROJECT = config("JIRA_URL_PROJECT")
 API_URL_ISSUES = config("JIRA_URL_ISSUES")
+JIRA_URL_USERS = config("JIRA_URL_USERS")
 API_USER_NAME = config("JIRA_USER_NAME")
 API_TOKEN = config("JIRA_TOKEN")
 MONGO_PATH = config("MONGO_PATH")
@@ -19,6 +20,7 @@ MONGO_PATH = config("MONGO_PATH")
 client = MongoClient(MONGO_PATH)
 db = client["swag"]
 project_collections = db["projects_per_hours"]
+users_collection = db["users"]
 
 def get_api_data_project(user_name, token):
     response = requests.get(
@@ -208,3 +210,97 @@ def get_project_per_author(request):
             return JsonResponse({"error": "JSON inválido"}, status=400)
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
+        
+def get_api_data_users(user_name, token):
+        response = requests.get(
+            f"{JIRA_URL_USERS}",
+            auth=HTTPBasicAuth(user_name, token)
+        )
+        response.raise_for_status()
+        return response.json()
+
+@csrf_exempt
+def get_users(request):
+    if request.method != 'POST':
+        return JsonResponse(
+            {"error": "Método não permitido. Por favor, use POST."}, 
+            status=405
+        )
+
+    try:
+        data = json.loads(request.body)
+    except Exception:
+        return JsonResponse({"error": "JSON inválido"}, status=400)
+
+    user_name = data.get("user_name")
+    token = data.get("token")
+
+    if not user_name or not token:
+        return JsonResponse(
+            {"error": "Campos 'user_name' e 'token' são obrigatórios."}, 
+            status=400
+        )
+
+    try:
+        users_data = get_api_data_users(user_name, token)
+
+        users_collection.delete_many({})
+
+        if users_data:
+            users_collection.insert_many(users_data)
+
+        return JsonResponse(
+            {"message": "Usuários salvos com sucesso!"}, status=200
+        )
+
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({"error": f"Erro ao acessar API Jira: {str(e)}"}, status=500)
+    except Exception as e:
+        return JsonResponse({"error": f"Ocorreu um erro inesperado: {str(e)}"}, status=500)
+
+
+def list_users(request):
+    if request.method != 'GET':
+        return JsonResponse({"error": "Método não permitido. Use GET."}, status=405)
+    
+    try:
+        client = MongoClient(MONGO_PATH)
+        db = client["swag"]
+
+        filters = {}
+
+        account_id = request.GET.get('accountId')
+        if account_id:
+            filters['accountId'] = account_id
+
+        display_name = request.GET.get('displayName')
+        if display_name:
+            filters['displayName'] = {'$regex': display_name, '$options': 'i'}
+
+
+        users_collection = db["users"]
+        users = list(users_collection.find(filters, {"_id": 0, "accountId": 1, "displayName": 1}))
+        
+        return JsonResponse(users, safe=False)
+    
+    except Exception as e:
+        return JsonResponse({"error": f"Falha ao acessar os usuários: {e}"}, status=500)
+
+def list_user_by_Id(request, accountId):
+    if request.method != 'GET':
+        return JsonResponse({"error": "Método não permitido. Use GET."}, status=405)
+    
+    try:
+        client = MongoClient(MONGO_PATH)
+        db = client["swag"]
+
+        users_collection = db["users"]
+        user = users_collection.find_one({"accountId": accountId}, {"_id": 0,  "accountId": 1, "displayName": 1})
+
+        if user:
+            return JsonResponse(user, safe=False)
+        else:
+            return JsonResponse({"error": "Usuário não encontrado."}, status=404)
+        
+    except Exception as e:
+        return JsonResponse({"error": f"Falha ao acessar o usuário: {e}"}, status=500)
--- swag/swag/urls.py
+++ swag/swag/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-from nsnapp.views import save_data, get_project_per_period, get_project_per_author
+from nsnapp.views import get_users, list_user_by_Id, list_users, save_data, get_project_per_period, get_project_per_author
 
 def home(request):
     return HttpResponse("API Swag está no ar 🚀")
@@ -12,4 +12,7 @@ urlpatterns = [
     path('project/save_project', save_data),
     path('project/project_per_period', get_project_per_period),
     path('project/project_per_author', get_project_per_author),
+    path('users/save_users', get_users, name='get_users'),
+    path('users/list_users', list_users, name='list_users'),
+    path('users/<str:accountId>/', list_user_by_Id, name='list_user_by_accountId')
 ]
