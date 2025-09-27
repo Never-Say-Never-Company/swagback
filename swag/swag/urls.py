15e1b85d77a8e938f333627908b8b8a58f76a7a2
Vinicius Monteiro <vinicius3_1998@hotmail.com>
2025-09-26 22:57:03 -0300

    build(#14): arquivos para subir imagem no docker

--- swag/swag/urls.py
+++ swag/swag/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from django.urls import path  # <- path precisa estar aqui
+from django.urls import path
 from django.http import HttpResponse
 from nsnapp.views import save_data, get_project_per_period, get_project_per_author
 
-# view simples para a raiz
 def home(request):
     return HttpResponse("API Swag está no ar 🚀")
 
 urlpatterns = [
-    path('', home),  # rota para a raiz
+    path('', home),
     path('admin/', admin.site.urls),
     path('project/save_project', save_data),
     path('project/project_per_period', get_project_per_period),
