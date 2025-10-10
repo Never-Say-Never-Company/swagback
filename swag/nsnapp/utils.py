96a4a948c74a082d417f7cffcc7507e12e91d66c
Paloma <paloma.soaresrocha@gmail.com>
2025-10-10 13:39:39 -0300

    feat (#3): adicionando token e login do jira na secrets manager aws

--- swag/nsnapp/utils.py
+++ swag/nsnapp/utils.py
@@ -1,4 +1,7 @@
+import json
 from bson import ObjectId
+import boto3
+from botocore.exceptions import ClientError
 
 def convert_objectid_to_str(obj):
     if isinstance(obj, list):
@@ -9,3 +12,24 @@ def convert_objectid_to_str(obj):
         return str(obj)
     else:
         return obj
+    
+
+def get_secret(secret_name, region_name="sa-east-1"):
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    try:
+        return json.loads(secret)
+    except Exception:
+     return secret
