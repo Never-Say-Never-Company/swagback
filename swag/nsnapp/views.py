5e50d4be68c9765027360c61f4533ba0f37c9c47
Ricardo Campos <70415796+r1cardvs@users.noreply.github.com>
2025-10-26 10:05:11 -0300

    ajuste de formatação

--- swag/nsnapp/views.py
+++ swag/nsnapp/views.py
@@ -3,13 +3,13 @@ from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 import json
 import copy
-import requests 
+import requests
 
 
 from .services import (
-    save_data_service, 
-    get_project_by_period, 
-    get_project_by_author, 
+    save_data_service,
+    get_project_by_period,
+    get_project_by_author,
     filther_data,
     aggregate_project_periods_and_authors,
     list_users_service,
@@ -17,27 +17,27 @@ from .services import (
     count_issues_grouped_by_project_service,
     count_issues_by_user_and_total_hours_service,
     paginate_date_service,
-    save_developer_rates_service, 
-    list_developer_rates_service  
+    save_developer_rates_service,
+    list_developer_rates_service,
 )
 from nsnapp.utils import convert_objectid_to_str
-
-
-
 
 
 @csrf_exempt
 def save_data(request):
     if request.method == "POST":
         try:
-            
+
             result = save_data_service()
             return JsonResponse(result)
         except requests.exceptions.RequestException as e:
             return JsonResponse({"error": str(e)}, status=500)
         except Exception as e:
-            return JsonResponse({"error": f"Erro interno ao salvar dados: {e}"}, status=500)
-    
+            return JsonResponse(
+                {"error": f"Erro interno ao salvar dados: {e}"}, status=500
+            )
+
+
 @csrf_exempt
 def get_project_per_period(request):
     if request.method == "POST":
@@ -51,16 +51,19 @@ def get_project_per_period(request):
             end = data.get("end")
 
             if not begin or not end:
-                return JsonResponse({"error": "Necessário ter 'begin' e 'end' nas chaves."}, status=400)
-            
+                return JsonResponse(
+                    {"error": "Necessário ter 'begin' e 'end' nas chaves."}, status=400
+                )
+
             result = list(get_project_by_period(begin, end))
             result = convert_objectid_to_str(result)
-            
+
             return JsonResponse(result, safe=False)
         except json.JSONDecodeError:
             return JsonResponse({"error": "JSON inválido"}, status=400)
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
+
 
 @csrf_exempt
 def get_project_per_author(request):
@@ -70,28 +73,32 @@ def get_project_per_author(request):
             data = json.loads(request.body)
 
             if not isinstance(data, list):
-                return JsonResponse({"error": "O corpo deve ser uma lista de autores."}, status=400)
-        
+                return JsonResponse(
+                    {"error": "O corpo deve ser uma lista de autores."}, status=400
+                )
+
             for item in data:
                 author = item.get("account_id")
 
                 if not author:
-                    return JsonResponse({"error": "Necessário ter 'account_id' na chave."}, status=400)
-            
+                    return JsonResponse(
+                        {"error": "Necessário ter 'account_id' na chave."}, status=400
+                    )
+
                 results = get_project_by_author(author)
                 results = convert_objectid_to_str(results)
 
                 for result in results:
-                    
-                    
-                    list_return.append(filther_data(copy.deepcopy(result), author)) 
-            
+
+                    list_return.append(filther_data(copy.deepcopy(result), author))
+
             return JsonResponse(list_return, safe=False)
         except json.JSONDecodeError:
             return JsonResponse({"error": "JSON inválido"}, status=400)
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
-        
+
+
 @csrf_exempt
 def get_project_per_period_and_author(request):
     if request.method != "POST":
@@ -105,45 +112,51 @@ def get_project_per_period_and_author(request):
         authors = data.get("authors")
 
         if not begin or not end:
-            return JsonResponse({"error": "Necessário ter 'begin' e 'end' nas chaves."}, status=400)
-        
-        if not isinstance(authors, list) or not authors:
-            return JsonResponse({"error": "O corpo deve conter uma lista não vazia de autores."}, status=400)
+            return JsonResponse(
+                {"error": "Necessário ter 'begin' e 'end' nas chaves."}, status=400
+            )
 
-        
+        if not isinstance(authors, list) or not authors:
+            return JsonResponse(
+                {"error": "O corpo deve conter uma lista não vazia de autores."},
+                status=400,
+            )
+
         list_return = aggregate_project_periods_and_authors(begin, end, authors)
-            
+
         return JsonResponse(list_return, safe=False)
 
     except json.JSONDecodeError:
         return JsonResponse({"error": "JSON inválido"}, status=400)
     except Exception as e:
-        print(f"Erro inesperado em get_project_per_period_and_author: {e}") 
+        print(f"Erro inesperado em get_project_per_period_and_author: {e}")
         return JsonResponse({"error": f"Erro interno no servidor: {e}"}, status=500)
-    
+
+
 def list_users(request):
-    if request.method != 'GET':
+    if request.method != "GET":
         return JsonResponse({"error": "Método não permitido. Use GET."}, status=405)
 
     try:
         filters = {}
 
-        account_id = request.GET.get('accountId')
+        account_id = request.GET.get("accountId")
         if account_id:
-            filters['accountId'] = account_id
+            filters["accountId"] = account_id
 
-        display_name = request.GET.get('displayName')
+        display_name = request.GET.get("displayName")
         if display_name:
-            filters['displayName'] = {'$regex': display_name, '$options': 'i'}
-        
+            filters["displayName"] = {"$regex": display_name, "$options": "i"}
+
         users = list_users_service(filters)
         return JsonResponse(users, safe=False)
 
     except Exception as e:
         return JsonResponse({"error": f"Falha ao acessar os usuários: {e}"}, status=500)
 
+
 def list_user_by_Id(request, accountId):
-    if request.method != 'GET':
+    if request.method != "GET":
         return JsonResponse({"error": "Método não permitido. Use GET."}, status=405)
 
     try:
@@ -157,8 +170,9 @@ def list_user_by_Id(request, accountId):
     except Exception as e:
         return JsonResponse({"error": f"Falha ao acessar o usuário: {e}"}, status=500)
 
+
 def count_issues_grouped_by_project(request):
-    if request.method != 'GET':
+    if request.method != "GET":
         return JsonResponse({"error": "Método não permitido. Use GET."}, status=405)
 
     try:
@@ -167,12 +181,12 @@ def count_issues_grouped_by_project(request):
 
     except Exception as e:
         return JsonResponse(
-            {"error": f"Falha ao contar issues por projeto: {e}"},
-            status=500
+            {"error": f"Falha ao contar issues por projeto: {e}"}, status=500
         )
 
+
 def count_issues_by_user_and_total_hours(request):
-    if request.method != 'GET':
+    if request.method != "GET":
         return JsonResponse({"error": "Método não permitido. Use GET."}, status=405)
 
     try:
@@ -181,42 +195,38 @@ def count_issues_by_user_and_total_hours(request):
 
     except Exception as e:
         return JsonResponse(
-            {"error": f"Falha ao contar issues por projeto: {e}"},
-            status=500
+            {"error": f"Falha ao contar issues por projeto: {e}"}, status=500
         )
-               
-@csrf_exempt 
+
+
+@csrf_exempt
 @require_http_methods(["POST"])
 def paginate_date(request):
-    if request.method != 'POST':
+    if request.method != "POST":
         return JsonResponse({"error": "Método não permitido. Use POST."}, status=405)
-    
+
     try:
         data = json.loads(request.body)
 
         if data is None:
             return JsonResponse({"error": "Dados inválidos"}, status=400)
-        
+
         init = data.get("init")
         end = data.get("end")
 
         if init is None or end is None:
-             return JsonResponse({"Error": "Necessário ter 'init' e 'end' no corpo."}, status=400)
-        
+            return JsonResponse(
+                {"Error": "Necessário ter 'init' e 'end' no corpo."}, status=400
+            )
+
         paginate_datas = paginate_date_service(init, end)
 
         return JsonResponse(paginate_datas, safe=False)
-    
+
     except ValueError as e:
         return JsonResponse({"Error": str(e)}, status=400)
     except Exception as e:
-        return JsonResponse(
-            {"error": f"Falha ao paginar dados: {e}"},
-            status=500
-        )
-        
-
-
+        return JsonResponse({"error": f"Falha ao paginar dados: {e}"}, status=500)
 
 
 @csrf_exempt
@@ -226,39 +236,45 @@ def save_developer_rates(request):
         data = json.loads(request.body)
 
         if not isinstance(data, list):
-            return JsonResponse({"error": "O corpo da requisição deve ser uma lista de desenvolvedores."}, status=400)
-        
-        
+            return JsonResponse(
+                {
+                    "error": "O corpo da requisição deve ser uma lista de desenvolvedores."
+                },
+                status=400,
+            )
+
         result = save_developer_rates_service(data)
 
-        return JsonResponse({
-            "status": "success", 
-            "message": result["message"],
-            "inserted": result["inserted"],
-            "modified": result["modified"]
-        }, status=200)
+        return JsonResponse(
+            {
+                "status": "success",
+                "message": result["message"],
+                "inserted": result["inserted"],
+                "modified": result["modified"],
+            },
+            status=200,
+        )
 
     except json.JSONDecodeError:
         return JsonResponse({"error": "JSON inválido na requisição."}, status=400)
     except ValueError as e:
-        
+
         return JsonResponse({"error": str(e)}, status=400)
     except Exception as e:
-        print(f"Erro inesperado em save_developer_rates: {e}") 
+        print(f"Erro inesperado em save_developer_rates: {e}")
         return JsonResponse({"error": f"Erro interno no servidor: {e}"}, status=500)
-        
-        
+
+
 @require_http_methods(["GET"])
 def list_developer_rates(request):
     """
     Novo endpoint GET para listar todos os documentos de taxas de desenvolvedores.
     """
     try:
-        
+
         rates_list = list_developer_rates_service()
-            
-        
+
         return JsonResponse(rates_list, safe=False, status=200)
 
     except Exception as e:
-        return JsonResponse({'error': f'Erro ao listar dados: {str(e)}'}, status=500)
+        return JsonResponse({"error": f"Erro ao listar dados: {str(e)}"}, status=500)
